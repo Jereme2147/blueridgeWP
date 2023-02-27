dc58042a7cc597105993f352dc9af685e21db55b
Jereme2147 <jereme.daniels@gmail.com>
2023-02-27 10:32:02 -0500

    top section done. need to do the JS for the disappearing logo next

---
+++ assets/moto-bbb.php
@@ -0,0 +1,17 @@
+<!-- this section includes the opening line, moto, service map for mobile, bbb stuff -->
+<section class="sectionStyle">
+        <div>
+        <h1 class="indexOpener">SERVING THE HIGH COUNTRY'S PROPANE NEEDS IN WESTERN NC AND EASTERN TN SINCE 1992 </h1>
+        </div>
+        <div class="serviceArea">
+            <img src="<?php echo get_template_directory_uri()?>/media/map.jpg" alt="">
+        </div>
+        <div class="moto">
+            <h3>Your only locally owned and operated full service propane company based in Avery County NC and servig Carter County TN by people you know from your community.</h3>
+        </div>
+        <div class="bbb">
+            <img src="<?php echo get_template_directory_uri()?>/media/bbb.jpg" alt="">
+            <h2>A+ Rated by the Better Business Bureau</h2>
+        </div>
+       
+    </section>
--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php 
 get_header();
 ?>
-    <section class="sectionStyle">
-        <div>
-        <h1 class="indexOpener">SERVING THE HIGH COUNTRY'S PROPANE NEEDS IN WESTERN NC AND EASTERN TN SINCE 1992 </h1>
-        </div>
-        <div class="serviceArea">
-
-        </div>
-        <div class="moto">
-            <h3>Your only locally owned and operated full service propane company based in Avery County NC and servig Carter County TN by people you know from your community.</h3>
-        </div>
-    </section>
+    <?php
+        require_once(plugin_dir_path(__FILE__) . '/assets/moto-bbb.php');
+    ?>
 </body>
 </html>
